6b1ad3f3bfbd9181d6a1875b959ae9eb790d1d39
Cecilia Goyenola <cecigoyenola@hotmail.com>
2017-04-27 22:35:08 +0200

    Now ht-p plots different indents in different colors

--- ht-p.py
+++ ht-p.py
@@ -2,37 +2,37 @@
 """Calculate hardness and elastic modulus from UMIS output."""
 
 import argparse
+from itertools import groupby, islice
+from operator import itemgetter
 
 from matplotlib import pyplot as plt
-import numpy
+
+
+def parseline(line):
+    indent, depth, load = line.split(",")
+    return (indent, float(depth), float(load))
 
 
 def main():
     parser = argparse.ArgumentParser(description=__doc__)
     parser.add_argument(
         "loadfile",
-        type=argparse.FileType("rb"),
+        type=argparse.FileType("r"),
         help="UMIS output file containing penetration and load data",
     )
     args = parser.parse_args()
 
-    i, h, P = numpy.genfromtxt(
-        args.loadfile,
-        delimiter=",",
-        skip_header=1,
-        usecols=(0,1, 2),
-        unpack=True,
-    )
+    lines = islice(args.loadfile, 1, None)
+    data = map(parseline, lines)
+    for indent, subdata in groupby(data, itemgetter(0)):
+        _, depth, load = zip(*subdata)
+        plt.plot(depth, load)
 
-    # i: indent
-    # h: penetration
-    # P: load
-
-    plt.plot(h, P, "k.")
-    plt.ylabel("Load (mN)")
     plt.xlabel("Penetration depth (um)")
+    plt.ylabel("Load (mN)")
 
     plt.show()
+
 
 if __name__ == "__main__":
     main()
